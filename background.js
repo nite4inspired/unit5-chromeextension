2f6bfc24951b29368f2a7ce9b6006ddc9216ef36
Samuel Kim <samuelkyuekim@gmail.com>
2023-07-21 16:57:26 -0400

    added addSite button and Done button

--- background.js
+++ background.js
@@ -1,12 +1,25 @@
-chrome.alarms.onAlarm.addListener(async (alarm) => {
+// chrome.alarms.onAlarm.addListener(async (alarm) => {
+//   if (alarm.name === 'closeTabs') {
+//     const tabs = await chrome.tabs.query({
+//       url: urls
+//     });
+//     const tabIds = tabs.map(({ id }) => id);
+//     chrome.tabs.remove(tabIds);
+//     document.querySelector('p').remove();
+//   }
+// });
+
+chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === 'closeTabs') {
-    const tabs = await chrome.tabs.query({
-      url: [
-        'https://www.youtube.com/*'
-      ]
+    // Retrieve the urls array from storage
+    chrome.storage.local.get(['urls'], async function(result) {
+      if (result.urls) {
+        const tabs = await chrome.tabs.query({
+          url: result.urls
+        });
+        const tabIds = tabs.map(({ id }) => id);
+        chrome.tabs.remove(tabIds);
+      }
     });
-    const tabIds = tabs.map(({ id }) => id);
-    chrome.tabs.remove(tabIds);
-    document.querySelector('p').remove();
   }
 });

43a0cd58b564826450c6a4131d726b82bcfed09e
Samuel Kim <samuelkyuekim@gmail.com>
2023-07-22 12:08:49 -0400

    Final Version

--- popup.js
+++ popup.js
@@ -19,7 +19,6 @@ chrome.storage.local.get(['urls'], function(result) {
 
 buttonSite.addEventListener('click', () => {
   urls.push(inputSite.value + '*');
-  console.log(urls);
 
   // Save the urls array to storage whenever it is updated
   chrome.storage.local.set({urls: urls}, function() {
@@ -36,14 +35,11 @@ buttonTime.addEventListener('click', () => {
   buttonTime.after(p);
 
   then.setHours(Number(time.split(':')[0]), Number(time.split(':')[1]), 0, 0); //
-  console.log(now);
-  console.log(then);
 
   if (now.getTime() > then.getTime()) {
     then.setDate(now.getDate() + 1); // If it's past 8:00 PM, schedule for next day
   }
   const delayInMinutes = (then.getTime() - now.getTime()) / (1000 * 60);
-  console.log(delayInMinutes);
   // Create the alarm
   chrome.alarms.create('closeTabs', { when: Date.now() + delayInMinutes * 60 * 1000 });
 })
@@ -52,7 +48,6 @@ buttonDone.addEventListener('click', async function() {
   const tabs = await chrome.tabs.query({
     url: urls
   });
-  console.log(urls);
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/Collator
   const collator = new Intl.Collator();
   tabs.sort((a, b) => collator.compare(a.title, b.title));
@@ -79,13 +74,21 @@ buttonDone.addEventListener('click', async function() {
   document.querySelector('ul').append(...elements);
 
   //const button = document.querySelector('button');
-  buttonTime.addEventListener('click', async () => {
+  //buttonTime.addEventListener('click', async () => {
     const tabIds = tabs.map(({ id }) => id);
     if (tabIds.length) {
       const group = await chrome.tabs.group({ tabIds });
       await chrome.tabGroups.update(group, { title: '⏰' });
     }
-  });
+  //});
 })
 
 
+
+const resetButton = document.getElementById('resetButton');
+
+resetButton.addEventListener('click', () => {
+  chrome.storage.local.clear(function() {
+    console.log('Local storage has been cleared');
+  });
+});
